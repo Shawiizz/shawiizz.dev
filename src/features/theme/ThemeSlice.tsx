c20b026e7ea6906ae5e77366cd0841769ae61599
Shawiiz_z <shaweereso@gmail.com>
2025-04-22 22:50:32 +0200

    fix: build

--- src/features/theme/ThemeSlice.tsx
+++ src/features/theme/ThemeSlice.tsx
@@ -1,6 +1,6 @@
-import { AppState } from '@/util/redux/Store'
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { ThemeMode } from 'flowbite-react/lib/esm/hooks/use-theme-mode'
+import {AppState} from '@/util/redux/Store'
+import {createSlice, PayloadAction} from '@reduxjs/toolkit'
+import {ThemeMode} from "flowbite-react";
 
 export interface ThemeState {
     mode: ThemeMode;
@@ -20,7 +20,7 @@ export const themeSlice = createSlice({
     }
 })
 
-export const { setMode } = themeSlice.actions
+export const {setMode} = themeSlice.actions
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectTheme = (state: AppState) => state.theme.mode
